b8b6757db222a4d603881633d7279829dd89ebf8
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2025-07-30 23:24:48 +0530

    Arrays QuickSort

--- src/main/java/primitivetypes/Arrays.java
+++ src/main/java/primitivetypes/Arrays.java
@@ -1,6 +1,8 @@
 package primitivetypes;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Arrays {
@@ -26,9 +28,48 @@ public class Arrays {
         print(array);
     }
 
+    /* 6.0 - Quick Sort.
+     */
+    public static void quickSortHelper(ArrayList<Integer> array, int s, int e) {
+        if (s >= e) {
+            return;
+        }
+        int pI = new Random().nextInt(e - s + 1) + s;
+        int p = array.get(pI);
+        Collections.swap(array, s, pI);
+
+        int L = s + 1;
+        int R = e;
+
+        while (L <= R) {
+            if (array.get(L) < p) {
+                L++;
+            } else if (p < array.get(R)) {
+                R--;
+            } else {
+                Collections.swap(array, L++, R--);
+            }
+        }
+        Collections.swap(array, s, R);
+        quickSortHelper(array, s, R - 1);
+        quickSortHelper(array, R + 1, e);
+    }
+    public static void quickSort() {
+        ArrayList<Integer> al = getArrayList(10);
+        print(al);
+        quickSortHelper(al, 0, al.size() - 1);
+        print(al);
+    }
+
     public static void print(int[] array) {
         System.out.println();
         for (int i : array) {
+            System.out.print(i + ", ");
+        }
+    }
+    public static void print(List<Integer> l) {
+        System.out.println();
+        for (int i : l) {
             System.out.print(i + ", ");
         }
     }
@@ -47,8 +88,20 @@ public class Arrays {
         }
         return array;
     }
+    public static ArrayList<Integer> getArrayList(int size) {
+        ArrayList<Integer> al = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 0; i < size; i++) {
+            al.add(r.nextInt(50));
+        }
+        return al;
+    }
 
     public static void main(String[] args) {
+        quickSort();
+        if (true) {
+            return;
+        }
         orderEvenOdd();
     }
 }
