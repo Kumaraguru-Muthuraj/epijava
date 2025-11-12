91a0fa69fb9059cadea226d32eafa3791d180591
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2025-11-12 21:25:53 +0530

    14.2

--- src/main/java/epi/Sorting.java
+++ src/main/java/epi/Sorting.java
@@ -31,8 +31,52 @@ public class Sorting {
         List<Integer> inter =  intersect(l1, l2);
         epi.Arrays.print(inter);
     }
+
+    /**
+     * 14.2 - Merge 2 sorted arrays.
+     */
+    public static List<Integer> merge2SortedArray(List<Integer> a, List<Integer> b) {
+        int aLen = a.size(), bLen = b.size();
+        int mLen = aLen + bLen;
+        int tempMLen = bLen;
+        while (tempMLen-- > 0) {
+            a.add(Integer.MIN_VALUE);
+        }
+        System.out.println("aLen - " + a.size());
+
+        int wIdx = mLen - 1;
+        int aIdx = aLen - 1;
+        int bIdx = bLen - 1;
+        while (bIdx >= 0) {
+            if (a.get(aIdx) < b.get(bIdx)) {
+                a.set(wIdx, b.get(bIdx));
+                bIdx--;
+            } else {
+                a.set(wIdx, a.get(aIdx));
+                aIdx--;
+            }
+            wIdx--;
+        }
+
+        return a;
+    }
+    public static void testMerge() {
+        List<Integer> l1 = new ArrayList<Integer>(Arrays.asList(2, 4, 6, 7, 8, 8, 9));
+        List<Integer> l2 = Arrays.asList(3, 5, 8, 9, 12, 15);
+        List<Integer> merged =  merge2SortedArray(l1, l2);
+        epi.Arrays.print(merged);
+    }
+
     public static void main(String[] args) {
+        // 14.2 - Intersect
+        testMerge();
+
+        if (true) {
+            return;
+        }
+        // 14.1 - Intersect
         testIntersect();
+
     }
 
 }
