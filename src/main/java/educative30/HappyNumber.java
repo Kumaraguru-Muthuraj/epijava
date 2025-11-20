215c40386c3c706cf8528318bae6071c01f750bd
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2025-11-21 00:51:47 +0530

    Happy Number

---
+++ src/main/java/educative30/HappyNumber.java
@@ -0,0 +1,83 @@
+package educative30;
+
+import java.util.HashMap;
+
+/**
+ * Write an algorithm to determine if a number n is a happy number.
+ * We use the following process to check if a given number is a happy number:
+ * Starting with the given number n, replace the number with the sum of the squares of its digits.
+ * Repeat the process until:
+ * The number equals 1, which will depict that the given number n is a happy number. The number enters a cycle,
+ * which will depict that the given number n is not a happy number. Return TRUE if n is a happy number, and FALSE if not.
+ */
+public class HappyNumber {
+    public static HashMap<Long, Long> map = new HashMap<>();
+    public static boolean isHappyNumber(int n) {
+        long goldenN = n;
+        long nCopy = n;
+        if (nCopy == 1) {
+            return true;
+        }
+        if (nCopy < 1) {
+            return false;
+        }
+
+        long res = 0;
+        while (true) {
+            res = sumOfSquares(nCopy);
+            if (map.containsKey(nCopy) && map.get(nCopy) != 1) {
+                return false;
+            } else {
+                map.put(nCopy, res);
+            }
+            if (goldenN == res) {
+                return false;
+            } else if (res == 1) {
+                return true;
+            }
+            nCopy = res;
+        }
+    }
+
+    public static long sumOfSquares(long n) {
+        if (map.get(n) != null) {
+            return map.get(n);
+        }
+        long rever = reverse(n);
+        if (map.get(rever) != null) {
+            return map.get(rever);
+        }
+        int res = 0;
+        while (n > 0) {
+            rever = reverse(n);
+            if (map.get(n) != null) {
+                res += map.get(n);
+            } else if (map.get(rever) != null) {
+                res += map.get(rever);
+            } else {
+                long lsd = n % 10;
+                res += lsd * lsd;
+            }
+            n /= 10;
+        }
+        return res;
+    }
+
+    public static long reverse(Long n) {
+        long rev = 0;
+        long lsd = 0;
+        while (n > 0) {
+            lsd = n % 10;
+            rev = rev * 10 + lsd;
+            n /= 10;
+        }
+        return rev;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isHappyNumber(2147483647));
+        //System.out.println(sumOfSquares(85));
+    }
+
+}
---
+++ src/main/java/educative30/HappyNumberV2.java
@@ -0,0 +1,43 @@
+package educative30;
+
+import java.util.HashMap;
+
+/**
+ * Write an algorithm to determine if a number n is a happy number.
+ * We use the following process to check if a given number is a happy number:
+ * Starting with the given number n, replace the number with the sum of the squares of its digits.
+ * Repeat the process until:
+ * The number equals 1, which will depict that the given number n is a happy number. The number enters a cycle,
+ * which will depict that the given number n is not a happy number. Return TRUE if n is a happy number, and FALSE if not.
+ */
+public class HappyNumberV2 {
+    public static boolean isHappyNumber(int n) {
+        int slow = n;
+        int fast = n;
+        do {
+            slow = sumOfSquares(slow);
+            fast = sumOfSquares(sumOfSquares(fast));
+            if (fast == 1)
+                return true;
+
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private static int sumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isHappyNumber(2147483647));
+        //System.out.println(sumOfSquares(85));
+    }
+
+}
