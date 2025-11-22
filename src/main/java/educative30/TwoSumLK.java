4b4fc561adb528aef64a600e657071905fd671a8
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2025-11-22 16:06:31 +0530

    2Sum

--- src/main/java/educative30/TwoSumLK.java
+++ src/main/java/educative30/TwoSumLK.java
@@ -1,2 +1,48 @@
-package educative30;public class TwoSumLK {
+package educative30;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of integers, nums, and an integer k, find the maximum sum of two elements in nums less than k.
+ * Otherwise, return −1, if no such pair exists.
+ */
+public class TwoSumLK {
+    public static int twoSumLessThanK(int[] nums, int k) {
+        Arrays.sort(nums);
+        epi.Arrays.print(nums);
+        int min = 0, max = nums.length - 1;
+        int sum = 0;
+        int prevSum = 0;
+        boolean found = false;
+        while (min < max) {
+            int minInt = nums[min];
+            int maxInt = nums[max];
+            sum = minInt + maxInt;
+            if (sum < k) {
+                prevSum = Math.max(sum, prevSum);
+                found = true;
+                min++;
+            } else {
+                prevSum = Math.min(sum, prevSum);
+                max--;
+            }
+        }
+        return found ? prevSum : -1;
+    }
+
+    public static void main(String[] args) {
+        //int[] a = {1, 2, 3, 4, 8, 9, 10};
+        //int[] a = {4,2,11,2,5,3,5,8};
+        int[] a = {13,22,28,47,52,66,79,99,109,126,135,139,143,153,153,155,183,207,208,209,215,220,224,226,245,273,274,285,288,289,326,331,338,344,349,361,378,380,393,419,440,444,466,489,490,496,498,503,508,513,529,556,557,567,570,587,589,590,607,615,616,618,624,631,669,691,700,702,722,753,766,769,772,788,790,792,812,848,861,862,865,886,900,901,914,918,919,925,953,954,955,960,967,971,978,985,987,988,989,991};
+        System.out.println(twoSumLessThanK(a, 749));
+        //[4,2,11,2,5,3,5,8] , 7 - Exp - 6
+        //[34,23,1,24,75,33,54,8] , 60 - Exp - 58
+        /**
+         * int[] a = {13,22,28,47,52,66,79,99,109,126,135,139,143,153,153,155,183,207,208,209,215,220,224,226,245,273,274,285,288,289,326,331,338,
+         * 344,349,361,378,380,393,419,440,444,466,489,490,496,498,503,508,513,529,556,557,567,570,587,589,590,607,615,616,618,624,631,669,691,700,702,
+         * 722,753,766,769,772,788,790,792,812,848,861,862,865,886,900,901,914,918,919,925,953,954,955,960,967,971,978,985,987,988,989,991};
+         * 749
+         * Res - 748
+         */
+    }
 }
--- src/main/java/educative30/TwoSumLKV2.java
+++ src/main/java/educative30/TwoSumLKV2.java
@@ -6,43 +6,61 @@ import java.util.Arrays;
  * Given an array of integers, nums, and an integer k, find the maximum sum of two elements in nums less than k.
  * Otherwise, return −1, if no such pair exists.
  */
-public class TwoSumLK {
-    public static int twoSumLessThanK(int[] nums, int k) {
-        Arrays.sort(nums);
-        epi.Arrays.print(nums);
-        int min = 0, max = nums.length - 1;
-        int sum = 0;
-        int prevSum = 0;
-        boolean found = false;
-        while (min < max) {
-            int minInt = nums[min];
-            int maxInt = nums[max];
-            sum = minInt + maxInt;
-            if (sum < k) {
-                prevSum = Math.max(sum, prevSum);
-                found = true;
-                min++;
+public class TwoSumLKV2 {
+    private static int search(int[] nums, int target, int start) {
+        int left = start, right = nums.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                result = mid;
+                left = mid + 1;
             } else {
-                prevSum = Math.min(sum, prevSum);
-                max--;
+                right = mid - 1;
             }
         }
-        return found ? prevSum : -1;
+
+        return result;
     }
 
+    public int twoSumLessThanK(int[] nums, int k) {
+        int maximumSum = -1;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            int j = search(nums, k - nums[i], i + 1);
+
+            if (j > i) {
+                maximumSum = Math.max(maximumSum, nums[i] + nums[j]);
+            }
+        }
+
+        return maximumSum;
+    }
+
+    // Driver code
     public static void main(String[] args) {
-        //int[] a = {1, 2, 3, 4, 8, 9, 10};
-        //int[] a = {4,2,11,2,5,3,5,8};
-        int[] a = {13,22,28,47,52,66,79,99,109,126,135,139,143,153,153,155,183,207,208,209,215,220,224,226,245,273,274,285,288,289,326,331,338,344,349,361,378,380,393,419,440,444,466,489,490,496,498,503,508,513,529,556,557,567,570,587,589,590,607,615,616,618,624,631,669,691,700,702,722,753,766,769,772,788,790,792,812,848,861,862,865,886,900,901,914,918,919,925,953,954,955,960,967,971,978,985,987,988,989,991};
-        System.out.println(twoSumLessThanK(a, 749));
-        //[4,2,11,2,5,3,5,8] , 7 - Exp - 6
-        //[34,23,1,24,75,33,54,8] , 60 - Exp - 58
-        /**
-         * int[] a = {13,22,28,47,52,66,79,99,109,126,135,139,143,153,153,155,183,207,208,209,215,220,224,226,245,273,274,285,288,289,326,331,338,
-         * 344,349,361,378,380,393,419,440,444,466,489,490,496,498,503,508,513,529,556,557,567,570,587,589,590,607,615,616,618,624,631,669,691,700,702,
-         * 722,753,766,769,772,788,790,792,812,848,861,862,865,886,900,901,914,918,919,925,953,954,955,960,967,971,978,985,987,988,989,991};
-         * 749
-         * Res - 748
-         */
+        int[][] nums = {
+                {4, 5, 2, 6, 2},
+                {4, 9, 5, 2, 5, 6, 8, 1, 4, 7, 9},
+                {10, 20, 30},
+                {34, 23, 1, 24, 75, 33, 54, 8},
+                {5, 5, 5, 5, 5, 5, 5, 5}
+        };
+
+        int[] k = {12, 14, 15, 54, 10};
+
+        TwoSumLKV2 obj = new TwoSumLKV2();
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print((i + 1) + ".\tnums: [");
+            for (int j = 0; j < nums[i].length; j++) {
+                System.out.print(nums[i][j]);
+                if (j < nums[i].length - 1) System.out.print(", ");
+            }
+            System.out.println("]\n\tk: " + k[i]);
+            System.out.println("\n\tTwo sum less than k: " + obj.twoSumLessThanK(nums[i], k[i]));
+            System.out.println(new String(new char[100]).replace('\0', '-'));
+        }
     }
 }
