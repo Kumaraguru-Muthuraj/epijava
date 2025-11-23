bc90e4474e33dacea8b421fc6a775709285d7442
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2025-11-23 18:32:11 +0530

    16.10 - Gray Code

--- src/main/java/epi/Recursion.java
+++ src/main/java/epi/Recursion.java
@@ -1,6 +1,41 @@
 package epi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recursion {
+    /** 16.10 - Gray code. Successive numbers have just 1 bit flipped.
+     * n - Number of bits.
+     */
+    public static List<Integer> grayCode(int n){
+        List<Integer> grayLst = new ArrayList<>();
+        if (n == 0) {
+            throw new RuntimeException("Error");
+        }
+        if (n == 1) {
+            grayLst.add(0);
+            grayLst.add(1);
+            return grayLst;
+        }
+        grayLst = grayCode(n-1);
+
+        int mask = 1 << n-1;
+        List<Integer> newLst = new ArrayList<>();
+        for (int i = grayLst.size() - 1; i >= 0 ; i--) {
+            Integer elem = grayLst.get(i);
+            newLst.add(elem ^ mask);
+        }
+        grayLst.addAll(newLst);
+        return grayLst;
+    }
+    public static void testGaryCode() {
+        int k = 4;
+        List<Integer> result = grayCode(k);
+        for (Integer r : result) {
+            System.out.printf("%d - %s%n", r, String.format("%" + k + "s", Integer.toBinaryString(r)).replace(' ', '0'));
+        }
+    }
+
     /** 16.11 - Diameter of a tree, may not me through root.
      */
     public static int[] diameter(Node2 root) {
@@ -28,8 +63,15 @@ public class Recursion {
         System.out.println("\nHeight - " + hAndD[0] + ", Diam - " + hAndD[1]);
     }
 
-
     public static void main(String[] args) {
+
+        if (true) {
+            return;
+        }
+        //16.10 - Gary code
+        testGaryCode();
+
+        //16.11 - Diameter
         testDiameter();
     }
 }
