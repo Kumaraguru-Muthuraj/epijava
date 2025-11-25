5b7140865cfb76589ec61d732ca26017589cc6c4
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2025-11-25 07:18:56 +0530

    Longest Common Suffix

---
+++ src/main/java/educative30/LongestCommonSuffix.java
@@ -0,0 +1,11 @@
+package educative30;
+
+/**
+ * You are given two arrays of strings, wordsContainer and wordsQuery.
+ * For each string wordsQuery[i], find the string in wordsContainer that shares the longest common suffix with it.
+ * If multiple strings in wordsContainer share the same longest suffix, choose the one with the smallest length.
+ * If two or more such strings have the same smallest length, choose the string that appears earliest in wordsContainer.
+ * Return an array of integers ans, where ans[i] is the index of the chosen string in wordsContainer for the query wordsQuery[i].
+ */
+public class LongestCommonSuffix {
+}
