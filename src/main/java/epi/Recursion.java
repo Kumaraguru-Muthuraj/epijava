bc983f272112223e02ed92ac337dbcf6c3cde3ed
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2025-12-02 23:22:41 +0530

    BInary trees distinct

--- src/main/java/epi/Recursion.java
+++ src/main/java/epi/Recursion.java
@@ -1,9 +1,48 @@
 package epi;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class Recursion {
+    /** 16.8 - Distinct binary trees.
+     * Time and Space - ??
+     */
+    public static List<Node2> distinctBinaryTrees(int numNodes) {
+        List<Node2> trees = new LinkedList<>();
+        Random r = new Random();
+        if (numNodes == 0) {
+            trees.add(null);
+        }
+        for (int numLTrees = 0; numLTrees < numNodes; numLTrees++) {
+            int numRTrees = numNodes - 1 - numLTrees;
+            List<Node2> lTrees = distinctBinaryTrees(numLTrees);
+            List<Node2> rTrees = distinctBinaryTrees(numRTrees);
+            for (Node2 lTree : lTrees) {
+                for (Node2 rTree : rTrees) {
+                    Node2 root = new Node2(r.nextInt(50));
+                    root.left = lTree;
+                    root.right = rTree;
+                    trees.add(root);
+                }
+            }
+        }
+
+        return trees;
+    }
+    public static void testBinTrees() {
+        List<Node2> trees = distinctBinaryTrees(4);
+        int treeCnt = 1;
+        for (Node2 tree : trees) {
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.root = tree;
+            System.out.println("\n\nTree Cnt - " + treeCnt++);
+            bst.print();
+        }
+    }
+
+
     /** 16.10 - Gray code. Successive numbers have just 1 bit flipped.
      * n - Number of bits.
      */
@@ -64,7 +103,8 @@ public class Recursion {
     }
 
     public static void main(String[] args) {
-
+        //16.8 - BInary Trees
+        testBinTrees();
         if (true) {
             return;
         }
